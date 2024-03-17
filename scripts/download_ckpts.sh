c3edb9613bbba34f8e963e43d48b1ff52fc89260
Yixuan Wang <yixuan22@illinois.edu>
2024-03-16 21:53:18 -0500

    update ckpts script

---
+++ scripts/download_ckpts.sh
@@ -0,0 +1,4 @@
+# ckpts for knife task in real world
+curl -L https://uofi.box.com/shared/static/akojvx5indl2fa81helkekcknnqz6zg2 --output latest.ckpt
+mkdir -p data/outputs/knife
+mv latest.ckpt data/outputs/knife
